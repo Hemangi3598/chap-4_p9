48ec455b2ff36fab9c4ae9eb1de6619000513f0d
Hemangi3598 <90692254+Hemangi3598@users.noreply.github.com>
2021-09-18 16:50:26 +0530

    Add files via upload

---
+++ p11.py
@@ -0,0 +1,9 @@
+# wapp to read username and password
+# if username is kamal and password is classes
+# then o/p should be succcess else failure
+username = input(" enter your username ")
+password = input(" enter your password ")
+if ( username == "kamal") and (password == "classes"):
+	print(" success")
+else:
+	print("failure")
